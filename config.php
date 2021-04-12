9fbace396da3ccac1e3cb7048a54ff1457a42597
Arpitha K V <arpitha6556@gmail.com>
2021-04-12 21:21:33 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id16586271_grip','hpxOp0v64j0@K/~u','id16586271_grip_funds');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
